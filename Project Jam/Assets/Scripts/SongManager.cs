2c34086141ffc36042c645009eefb3b37c22104d
jazmirafy <117775854+jazmirafy@users.noreply.github.com>
2025-07-11 12:17:50 -0400

    getting the basic midi function to work
    this doesnt have the full attack defend phase, multiplyer, and score screen but the basic midi functions work

---
+++ Project Jam/Assets/Scripts/SongManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
+using UnityEngine.Networking;
+
+public class SongManager : MonoBehaviour
+{
+    public static SongManager Instance; // access song manager from other classes
+    public AudioSource audioSource; // our music goes here
+    public float songDelayInSeconds; // delay our song after a certain amount of time
+    public int inputDelayInMilliseconds; // in case there's a keyboard issue and we have input delay
+    public string fileLocation; // place in StreamingAssets where the MIDI file is kept
+    public float noteTime; // how much time the note will be on screen
+    public float noteSpawnY; // where the notes spawn
+    public float noteTapY; // where the notes need to be tapped
+    public Lane[] lanes; // your custom class for input lanes
+    public double marginOfError; // in seconds
+
+    public float noteDespawnY
+    {
+        get
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+
+    public static MidiFile midiFile; // where the midi file is loaded into memory
+
+    void Start()
+    {
+        Instance = this;
+
+        if (Application.streamingAssetsPath.StartsWith("http://") || Application.streamingAssetsPath.StartsWith("https://"))
+        {
+            StartCoroutine(ReadFromWebsite());
+        }
+        else
+        {
+            ReadFromFile();
+        }
+    }
+
+    private IEnumerator ReadFromWebsite()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + fileLocation))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                byte[] results = www.downloadHandler.data;
+                using (var stream = new MemoryStream(results))
+                {
+                    midiFile = MidiFile.Read(stream);
+                    GetDataFromMidi();
+                }
+            }
+        }
+    }
+
+    private void ReadFromFile()
+    {
+        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+        GetDataFromMidi();
+    }
+
+    public void GetDataFromMidi()
+    {
+        var notes = midiFile.GetNotes();
+        var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+        notes.CopyTo(array, 0);
+
+        foreach (var lane in lanes)
+        {
+            lane.SetTimeStamps(array); // this method will live in your Lane.cs
+        }
+
+        Invoke(nameof(StartSong), songDelayInSeconds);
+    }
+
+    public void StartSong()
+    {
+        audioSource.Play();
+    }
+
+    public static double GetAudioSourceTime()
+    {
+        return (double)Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
+    }
+
+    void Update()
+    {
+        // usually not needed unless you’re syncing other runtime logic
+    }
+}
